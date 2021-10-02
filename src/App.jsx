f08a45e00a947e4ad455ecabb5a88d53003a6246
jhs512 <jangka512@gmail.com>
2021-10-02 17:50:43 +0900

    텍스트 수정모드

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
 import { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("11");
+  const [editMode, setEditMode] = useState(false);
+  const [text, setText] = useState("안녕하세요.");
+  const [newText, setNewText] = useState(text);
 
-  return (
-    <div className="App">
-      <input
-        type="text"
-        onChange={({ target: { value } }) => {
-          setText(value);
-        }}
-        value={text}
-      />
+  const showEdit = () => {
+    setEditMode(true);
+  };
+
+  let content = (
+    <div>
+      {text}
+      <button onClick={showEdit}>수정모드로 변경</button>
     </div>
   );
+
+  if (editMode) {
+    const commitEdit = () => {
+      setEditMode(false);
+      setText(newText);
+    };
+
+    const cancelEdit = () => {
+      setEditMode(false);
+      setNewText(text);
+    };
+
+    content = (
+      <>
+        <input
+          type="text"
+          onChange={({ target: { value } }) => {
+            setNewText(value);
+          }}
+          value={newText}
+        />
+
+        <button onClick={cancelEdit}>수정취소</button>
+        <button onClick={commitEdit}>수정완료</button>
+      </>
+    );
+  }
+
+  return <div className="App">{content}</div>;
 }
 
 export default App;
